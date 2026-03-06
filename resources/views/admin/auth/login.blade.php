13ec0458276b4710e0c96e1cb188b04176002aea
weblithestudio <weblithestudio@gmail.com>
2026-03-06 13:24:12 +0100

    Admin login

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,49 +1,61 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+@extends('admin.layouts.master')
 
-    <h2>Admin Bejelentkezés</h2>
+@section('content')
+<section class="section">
+    <div class="container container-login">
+        <div class="row justify-content-center">
+            <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
+                <div class="card card-primary border-box">
+                    <div class="card-header card-header-auth text-center">
+                        <h4>Admin Bejelentkezés</h4>
+                    </div>
 
-    <form method="POST" action="{{ route('admin.login') }}">
-        @csrf
+                    <div class="card-body card-body-auth">
+                        <!-- Session Status -->
+                        <x-auth-session-status class="mb-4" :status="session('status')" />
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                        <!-- Login Form -->
+                        <form method="POST" action="{{ route('admin.login') }}">
+                            @csrf
+
+                            <!-- Email -->
+                            <div class="form-group">
+                                <input id="email" type="email" name="email" placeholder="Email Address"
+                                    class="form-control @error('email') is-invalid @enderror"
+                                    value="{{ old('email') }}" required autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+
+                            <!-- Password -->
+                            <div class="form-group">
+                                <input id="password" type="password" name="password" placeholder="Password"
+                                    class="form-control @error('password') is-invalid @enderror" required>
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+
+                            <!-- Submit -->
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary btn-lg w-100">
+                                    Bejelentkezés
+                                </button>
+                            </div>
+
+                        </form>
+                        <!-- End Login Form -->
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('admin.password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('admin.password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </div>
+</section>
+@endsection
---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+
+    <link rel="icon" type="image/png" href="uploads/favicon.png">
+
+    <title>Admin Panel</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet">
+
+    @include('admin.layouts.styles')
+
+    @include('admin.layouts.scripts')
+</head>
+
+<body>
+<div id="app">
+    <div class="main-wrapper">
+        @yield('content')
+    </div>
+</div>
+
+ @include('admin.layouts.scripts_bottom')
+
+</body>
+</html>
